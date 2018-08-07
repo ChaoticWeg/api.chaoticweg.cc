8c09bda98e86b0363cef78373363e083510b9d8d
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-07 05:05:36 +0000

    add logging

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 
 var express = require('express');
+
+var logging = require('./lib/logging.js');
 var routes  = require('./routes');
 
 var app     = express();
 
+// middleware
+app.use(logging);
+
+// routes
 app.use('/slack', routes.slack);
 
+// hey! listen!
 app.listen(process.env.PORT, () => {
     console.log('listening on port ' + process.env.PORT);
 });
