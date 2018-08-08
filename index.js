30576cb905269ba00e8d58b8add220a617bbe316
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-08 05:13:53 +0000

    add explicit method restriction

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const DEFAULT_PORT = 3000;
+const ALLOWED_METHODS = [ 'GET', 'POST' ];
 
 require('dotenv').config();
 
 var express = require('express');
-
-var logging = require('./lib/logging.js');
 var routes  = require('./routes');
 
 var app     = express();
@@ -12,7 +11,8 @@ var app     = express();
 app.set('port', process.env.PORT || DEFAULT_PORT);
 
 // middleware
-app.use(logging);
+app.use(require('./lib/logging.js'));
+app.use(require('./lib/restrict-methods.js'));
 
 // routes
 app.get('*', (req, res) => { res.redirect(301, '//chaoticweg.cc'); });
